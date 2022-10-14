a268dfbefede18ee88a7d65c3f1ce7a61cd0a917
juanes <juan.epc2000@gmail.com>
2022-10-14 16:08:56 -0500

    hasta aca llegue con autores jaja

--- view/Autores.php
+++ view/Autores.php
@@ -248,14 +248,14 @@ switch ($btn) {
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">Evidencia</h5>
+                        <h5 class="modal-title" id="staticBackdropLabel">Autor</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="row">
                             <div class="row g-3">
                                 <div class="col">
-                                    <label>ID Evidencia</label>
+                                    <label>ID autor</label>
                                     <input class="form-control" type="text" name="ID_EVIDENCIA"
                                         value="<?php echo $idEvi ?>">
                                 </div>
